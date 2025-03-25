410714deeaaaac50df2d2fe11e2548d85e19d33d
shinwoo park <turbospace@naver.com>
2025-03-25 23:49:39 +0900

    pipe 추가가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Delete, Patch, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Patch,
+  Param,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -6,33 +16,32 @@ import { CreateBoardDto } from './dto/create-board.dto';
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
-  
+
   @Get('/')
   getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
   }
 
   @Post()
-  createBoard(
-    @Body() createBoardDto: CreateBoardDto
-  ): Board {
-      return this.boardsService.createBoard(createBoardDto);
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
   }
 
   @Get('/:id')
-  getBoardById(@Param('id') id:string): Board | undefined {
+  getBoardById(@Param('id') id: string): Board | undefined {
     return this.boardsService.getBoardById(id);
   }
 
   @Delete('/:id')
-  deleteBoard(@Param('id') id: string) : void {
+  deleteBoard(@Param('id') id: string): void {
     return this.boardsService.deleteBoard(id);
   }
 
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus
+    @Body('status') status: BoardStatus,
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
