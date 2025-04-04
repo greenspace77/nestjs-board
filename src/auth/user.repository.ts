66246530bfae2853773c1e613e28f616c7ed3f23
greenspace77 <greenspace@sk.com>
2025-04-04 10:21:26 +0900

    비밀번호 암호화 코드 추가

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,6 +5,7 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Injectable()
@@ -15,10 +16,10 @@ export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({
-      username,
-      password,
-    });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword});
 
     try {
       await this.save(user);
