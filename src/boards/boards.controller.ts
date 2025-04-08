0d85df8b83a55cdc7a256eafeea1c3b3e3131d16
shinwoo park <turbospace@naver.com>
2025-04-08 22:34:43 +0900

    로그 추가가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -10,6 +10,7 @@ import {
   ValidationPipe,
   ParseIntPipe,
   UseGuards,
+  Logger,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardStatus } from './board.status.enum';
@@ -23,6 +24,7 @@ import { GetUser } from 'src/auth/get-user-decorator';
 @Controller('boards')
 @UseGuards(AuthGuard())
 export class BoardsController {
+  private logger = new Logger('BoardsController');
   constructor(private boardsService: BoardsService) {}
 
   // @Get('/')
@@ -32,6 +34,7 @@ export class BoardsController {
 
   @Get()
   getAllBoard(@GetUser() user: User): Promise<Board[]> {
+    this.logger.verbose(`user ${user.username} trying to get all boards`);
     return this.boardsService.getAllBoards(user);
   }
 
@@ -47,6 +50,8 @@ export class BoardsController {
     @Body() createBoardDto: CreateBoardDto,
     @GetUser() user: User,
   ): Promise<Board> {
+    this.logger.verbose(`User ${user.username} creating a new board.
+      Payload: ${JSON.stringify(createBoardDto)}`);
     return this.boardsService.createBoard(createBoardDto, user);
   }
 
